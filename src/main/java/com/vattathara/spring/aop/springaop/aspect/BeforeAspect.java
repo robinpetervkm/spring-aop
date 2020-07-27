d9d0ae1d26d13ca21bd8d9a8b6cc3e6b5ddb7705
Robinclear <robin.peter.vkm@gmail.com>
2020-07-27 15:43:21 -0300

    detailed info of called function

--- src/main/java/com/vattathara/spring/aop/springaop/aspect/BeforeAspect.java
+++ src/main/java/com/vattathara/spring/aop/springaop/aspect/BeforeAspect.java
@@ -3,6 +3,7 @@ package com.vattathara.spring.aop.springaop.aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -18,9 +19,8 @@ public class BeforeAspect {
 	//execution(* PACKAGE.*.*(..))
 	
 	@Before("execution(* com.vattathara.spring.aop.springaop.business.*.*(..))")
-	public void before() {
-		
-		LOGGER.info("Interceped Method calls {}");
+	public void before(JoinPoint joinPoint) {
+		LOGGER.info("Interceped Method calls {}",joinPoint);
 	}
 
 }
